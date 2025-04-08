c0486327d847007dd05ce8b7ae9c08ffa84f911a
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-08 19:44:43 -0400

    Versão ajustada

--- Programa de login/Login.java
+++ Programa de login/Login.java
@@ -1,51 +1,50 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+// Para executar rode: java -cp "Programa de login" Login 
 
-class LoginHandler implements ActionListener {
-  private JTextField userField;
-  private JPasswordField passwordField;
+public class Login extends JFrame {
+  private JTextField campoLogin;
+  private JPasswordField campoSenha;
+  private JButton botaoOK;
 
-  public LoginHandler(JTextField userField, JPasswordField passwordField) {
-    this.userField = userField;
-    this.passwordField = passwordField;
+  public Login() {
+    setTitle("Sistema de Login");
+    setSize(300, 200);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setLayout(new GridLayout(3, 2));
+
+    JLabel labelLogin = new JLabel("Login:");
+    campoLogin = new JTextField();
+    JLabel labelSenha = new JLabel("Senha:");
+    campoSenha = new JPasswordField();
+    botaoOK = new JButton("OK");
+
+    add(labelLogin);
+    add(campoLogin);
+    add(labelSenha);
+    add(campoSenha);
+    add(new JLabel());
+    add(botaoOK);
+
+    botaoOK.addActionListener(new ActionListener() {
+      public void actionPerformed(ActionEvent e) {
+        String login = campoLogin.getText();
+        String senha = new String(campoSenha.getPassword());
+
+        JOptionPane.showMessageDialog(null,
+            "Login: " + login + "\nSenha: " + senha,
+            "Credenciais",
+            JOptionPane.INFORMATION_MESSAGE);
+      }
+    });
   }
 
-  @Override
-  public void actionPerformed(ActionEvent e) {
-    String username = userField.getText();
-    String password = new String(passwordField.getPassword());
-
-    JOptionPane.showMessageDialog(null,
-        "Login: " + username + "\nSenha: " + password,
-        "Informações do Usuário",
-        JOptionPane.INFORMATION_MESSAGE);
-  }
-}
-
-public class Login {
   public static void main(String[] args) {
-    JFrame frame = new JFrame("Login");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(300, 150);
-    frame.setLayout(new GridLayout(3, 2));
-
-    JLabel userLabel = new JLabel("Usuário:");
-    JTextField userField = new JTextField();
-    JLabel passwordLabel = new JLabel("Senha:");
-    JPasswordField passwordField = new JPasswordField();
-    JButton loginButton = new JButton("OK");
-
-    frame.add(userLabel);
-    frame.add(userField);
-    frame.add(passwordLabel);
-    frame.add(passwordField);
-    frame.add(loginButton);
-
-    LoginHandler handler = new LoginHandler(userField, passwordField);
-    loginButton.addActionListener(handler);
-
-    frame.setVisible(true);
+    SwingUtilities.invokeLater(new Runnable() {
+      public void run() {
+        new Login().setVisible(true);
+      }
+    });
   }
 }
