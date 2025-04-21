010d87072bd2b299892e1e449b2933accbf710dd
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-21 18:40:14 -0400

    versão final com as classes prontas

--- OrientaçãoAObjeto/App.java
+++ OrientaçãoAObjeto/App.java
@@ -1,16 +1,7 @@
 package OrientaçãoAObjeto;
 
-import javax.swing.JFrame;
-
 public class App {
     public static void main(String[] args) throws Exception {
-
-        InterfaceUsuario tela = new InterfaceUsuario();
-
-        tela.showPlanos();
-        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tela.setSize(1000, 600); // configura o tamanho do fram
-        tela.setVisible(true); // exibe o frame
 
     }
 }
