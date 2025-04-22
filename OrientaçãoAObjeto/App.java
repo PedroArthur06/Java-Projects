9e679dba768f1fec6ecefa60a4a8c9852b5b7a38
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-22 09:41:23 -0400

    primeiro usuario criado

--- OrientaçãoAObjeto/App.java
+++ OrientaçãoAObjeto/App.java
@@ -3,5 +3,14 @@ package OrientaçãoAObjeto;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        Usuario usuario1 = new Usuario();
+        usuario1.setNome("Pedro arthur");
+        usuario1.setEmail("pedro123@gmail.com");
+        usuario1.setDataNascimento(2003);
+        usuario1.setSenha("123456789");
+        usuario1.setCpf("123.456.789-00");
+        usuario1.setTelefone("1234567890");
+        usuario1.salvar();
+
     }
 }
---
+++ OrientaçãoAObjeto/Usuario.java
@@ -0,0 +1,81 @@
+package OrientaçãoAObjeto;
+
+public class Usuario {
+  private String nome;
+  private String email;
+  private int dataNascimento;
+  private String senha;
+  private String cpf;
+  private String telefone;
+
+  public Usuario(String nome, String email, int dataNascimento, String senha, String cpf, String telefone) {
+    this.nome = nome;
+    this.email = email;
+    this.dataNascimento = dataNascimento;
+    this.senha = senha;
+    this.cpf = cpf;
+    this.telefone = telefone;
+  }
+
+  public Usuario() {
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public int getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(int dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+
+  public String getSenha() {
+    return senha;
+  }
+
+  public void setSenha(String senha) {
+    this.senha = senha;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+
+  public void salvar() {
+    System.out.println("Usuario salvo com sucesso!");
+    System.out.println("Nome: " + nome);
+    System.out.println("Email: " + email);
+    System.out.println("Data de Nascimento: " + dataNascimento);
+    System.out.println("Senha: " + senha);
+    System.out.println("CPF: " + cpf);
+    System.out.println("Telefone: " + telefone);
+  }
+
+}
