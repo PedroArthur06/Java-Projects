fa08e249448471d6d5977e31a528133ab8e7aadd
Pedro Arthur Rodrigues <pedroxxl917@gmail.com>
2025-04-22 15:06:44 -0400

    código atualizado

--- JButton/ButtonTest.java
+++ JButton/ButtonTest.java
@@ -1,11 +1,12 @@
 package JButton;
 
-import javax.swing.JFrame; 
-public class ButtonTest { 
-    public static void main(String[] args) { 
-        ButtonFrame buttonFrame = new ButtonFrame(); // cria ButtonFrame 
-        buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        buttonFrame.setSize(275, 110); // configura o tamanho do frame 
-        buttonFrame.setVisible(true); // exibe o frame 
-    } // fim de main 
+import javax.swing.JFrame;
+
+public class ButtonTest {
+    public static void main(String[] args) {
+        ButtonFrame buttonFrame = new ButtonFrame(); // cria ButtonFrame
+        buttonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        buttonFrame.setSize(400, 330); // configura o tamanho do frame
+        buttonFrame.setVisible(true); // exibe o frame
+    } // fim de main
 } // fim da classe ButtonTest
